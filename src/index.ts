6efe1320229d39c4daab84504f210c493b741431
Alan Cole <alan@mistyrobotics.com>
2018-11-12 15:44:01 -0700

    added ts-node for running typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { ContactBook } from './ContactBook-class';
 import { Contact } from './Contact-class';
+import readline from 'readline';
 
 const rolodex = new ContactBook();
 const alan = new Contact('Alan', '333-333-3333');
 const leRoi = new Contact('Le Roi', '999-999-9999');
+var answer = 'not answered yet.'
 
 rolodex.addContact(alan);
 rolodex.addContact(leRoi);
-rolodex.printContacts();
+
+let addContact = readline.createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+);
+
+addContact.question("\n*-*-*-*-*\nWould you like to add a new contact? (Y/N)", (response: string) => {
+    answer = response;
+    console.log(`Answer: ${answer}`);
+    rolodex.printContacts();
+    addContact.close()
+})
